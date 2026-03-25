86e1a677b88f098ddcc8cc9c152a1093754b7c7e
unknown <ozairk.work@gmail.com>
2026-03-25 14:20:20 +0500

    fixed cors 12

--- app.js
+++ app.js
@@ -11,9 +11,13 @@ app.set("trust proxy", 1);
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: "https://prime-inspire-clone.vercel.app",
-    credentials: true
-}))
+  origin: "https://prime-inspire-clone.vercel.app",
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
+app.options("*", cors());
 
 app.use('/api/auth', router)
 

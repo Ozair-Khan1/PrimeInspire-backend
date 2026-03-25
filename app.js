625423292085680ac1c98e9ab11e9999d10e603a
unknown <ozairk.work@gmail.com>
2026-03-25 13:58:10 +0500

    fixed cors 10

--- app.js
+++ app.js
@@ -13,6 +13,15 @@ app.use(cors({
     credentials: true
 }))
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://prime-inspire-clone.vercel.app");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.options('*', cors())
+
 app.use('/api/auth', router)
 
 module.exports = app

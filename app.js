08d4127b519ec6801faff2d339ebadd580afaa6c
unknown <ozairk.work@gmail.com>
2026-03-25 14:26:18 +0500

    fixed cors 13

--- app.js
+++ app.js
@@ -13,8 +13,6 @@ app.use(cookieParser())
 app.use(cors({
   origin: "https://prime-inspire-clone.vercel.app",
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
 app.options("*", cors());
